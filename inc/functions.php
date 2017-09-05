d1b3daf9b9df30b32738d19feada6be81a6c995a
Luiz <luiz.nicolat@hotmail.com>
2017-09-05 20:27:59 -0300

    Resolvido problema do insert, ajustado de button para input no login.php

--- inc/functions.php
+++ inc/functions.php
@@ -31,22 +31,30 @@ class Usuario{
 	}
 
 
-	function cadastraUsuario($dados){
-		$usuario 	= $dados['user'];
-		$senha	= md5($dados['senha']);//encripta com MD5
+	function cadastraUsuario($user, $senha){
+        $conexao = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+        
+		$usuario 	= $user;
+		$senha	= md5($senha);//encripta com MD5
 		//verificando se ja existe
-		$existe = mysqli_query("SELECT id FROM usuarios WHERE login = '$usuario'");
+        $queryexiste = "SELECT id FROM usuarios WHERE login = '$usuario'";
+		$existe = mysqli_query($conexao, $queryexiste);
 
 		if(mysqli_num_rows($existe) > 0){ // retornou mais que 0 então existe
+            echo $usuario;
+		    echo $senha;
 			echo "Usuário já existe!";
 		} else { //se não grava no banco
 			//gravação no banco, $ok retorna TRUE ou FALSE
-			$query = "INSERT INTO usuarios (login, senha) values ('$usuario','$senha')";
-            $conexao = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+			$query = "INSERT INTO usuarios (login, senha) values ('$usuario','$senha')";            
 			$ok = mysqli_query($conexao,$query);
             if($ok){
-				echo "Cadstrado com sucesso!";
+                            echo $usuario;
+		    echo $senha;
+				echo "Cadastrado com sucesso!";
 			} else {
+                            echo $usuario;
+		    echo $senha;
 				echo "Erro ao Cadastrar!";
 			}
 		}
@@ -76,5 +84,12 @@ class Usuario{
 	}
 
 } //final da classe
+class teste{
+    
+    function testasefunciona(){
+        $Chups = "Teste de Echo";
+        echo $Chups;
+    }
+}
 
 ?>
--- logado/cad_user.php
+++ logado/cad_user.php
@@ -4,24 +4,47 @@
 
 <?php include('../inc/header.php'); ?>
 
-<div class="wrap" style="padding-top:20px">
-	<?php
-    if($_POST){  
-        echo Usuario::cadastraUsuario($_POST);
-    } ?>    
+<div class="wrap" style="padding-top:20px">	  
+    <?php
+            if (isset($_POST['submit'])) {
+//
+//                            $ecoa = new teste();
+//            echo $ecoa->testasefunciona();
+//                $username = $_POST['username'];
+//
+//                if ($username == "letmein") {
+//
+//                print ("Welcome back, friend!");
+//
+//                }
+//                else {
+//
+//                print ("You're not a member of this site");         
+
+//            $ecoa = new teste();
+//            echo $ecoa->testasefunciona();
+//             
+                $user   = $_POST['user'];
+                $senha  = $_POST['senha'];
+
+                $cadastra = new Usuario();
+                echo $cadastra->cadastraUsuario($user,$senha);
+                
+                }
+             ?>
   
     <form action="cad_user.php" method="post">
 		<div class="input-group col-md-4">
 		  <label for="usr">Usuário:</label>
-		  <input type="text" class="form-control" id="user">			
+		  <input type="text" class="form-control" id="user" name="user">			
 		</div>
 		<div class="input-group col-md-4">
 		  <label for="senha">Senha:</label>
-		  <input type="text" class="form-control" id="senha">			
+		  <input type="text" class="form-control" id="senha" name="senha">			
 		</div>
 		<div class="input-group col-md-4" style="padding-top:5px">
-		  <button type="submit" class="btn btn-success"><span class="glyphicon glyphicon-ok" aria-hidden="true"></span>  Cadastrar</button>
-                                        
+        <input type="submit" name="submit" value="Cadastrar" class="btn btn-success">  
+<!--        <button type="submit" name="submit "class="btn btn-success"><span class="glyphicon glyphicon-ok" aria-hidden="true"></span> Cadastrar</button>                                        -->
                                                         
                     
 		</div>
